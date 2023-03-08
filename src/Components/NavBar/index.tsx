5fa5ebeca41f463ba645906065431ba08b2a4393
zizzhangjohn <zizzhangjohn@gmail.com>
2023-03-08 17:31:31 +0800

    add menu onclick collapes

--- src/Components/NavBar/index.tsx
+++ src/Components/NavBar/index.tsx
@@ -1,4 +1,4 @@
-import { useContext, useState } from 'react';
+import { useContext, useEffect, useRef, useState } from 'react';
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
@@ -9,13 +9,37 @@ import { LogoutModal } from '../LogoutModal';
 const NavBar = () => {
   const { user, setUser } = useContext(GlobalContext);
   const [logOutModalShow, setLogOutModalShow] = useState(false);
+  const [expanded, setExpanded] = useState(false);
+  const toggleRef = useRef<HTMLButtonElement>(null);
+  const mobileMenuRef = useRef<HTMLDivElement>(null);
+  /*
+  click outside to close mobilemenu, excluding toggle
+  */
+  useEffect(() => {
+    let mouseClickHandler = (e: MouseEvent) => {
+      if (!toggleRef.current?.contains(e.target as Node) &&
+        !mobileMenuRef.current?.contains(e.target as Node)) {
+        setExpanded(false);
+      }
+    }
+    document.addEventListener("mousedown", mouseClickHandler);
+    return () => {
+      document.removeEventListener("mousedown", mouseClickHandler);
+    }
+  })
+
   return (
-    <Navbar className='fs-5' collapseOnSelect expand="lg" bg="dark" variant="dark">
+    <Navbar className='fs-5' expanded={expanded} expand="lg" bg="dark" variant="dark">
       <Container>
         <Navbar.Brand className='fs-3' as={NavLink} to="/">RPG-Battle</Navbar.Brand>
-        <Navbar.Toggle aria-controls="responsive-navbar-nav" />
-        <Navbar.Collapse>
-          <Nav className="me-auto">
+        <Navbar.Toggle onClick={() => setExpanded(pre => !pre)} aria-controls="responsive-navbar-nav" ref={toggleRef} />
+        <Navbar.Collapse ref={mobileMenuRef}>
+          <Nav className="me-auto" onClick={(e) => {
+            e.stopPropagation();
+            if (expanded) {
+              setExpanded(false);
+            }
+          }}>
             <Nav.Link as={NavLink} to="/">Home</Nav.Link>
             <Nav.Link as={NavLink} to="fight">Fight</Nav.Link>
             <Nav.Link as={NavLink} to="characters">Characters</Nav.Link>
